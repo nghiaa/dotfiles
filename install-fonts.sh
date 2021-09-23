3206073cc560a59b30b3cd47265bb868ec3d540a
nghiaa <nghiatruong.dev@gmail.com>
2021-09-23 17:34:33 +0700

    remove font temp before downloading again

--- install-fonts.sh
+++ install-fonts.sh
@@ -1,30 +1,37 @@
 #!/bin/sh
-sudo apt install -y fontconfig
+
+font_dir="$HOME/.fonts"
+temp="$HOME/powerline-font-temp"
 
 download-meslo-fonts() {
-    wget -P $HOME/powerline-font-temp https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
-    wget -P $HOME/powerline-font-temp https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf
-    wget -P $HOME/powerline-font-temp https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf
-    wget -P $HOME/powerline-font-temp https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
+    wget -P "$temp" https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf
+    wget -P "$temp" https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf
+    wget -P "$temp" https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf
+    wget -P "$temp" https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
 }
 
 install-powerline-fonts() {
-    if [[ ! -d "$HOME/.fonts" ]]; then
-        echo "Creating ~/.fonts"
-        mkdir "$HOME/.fonts"
-    fi
+    sudo apt install -y fontconfig
+
+    echo "Cleaning existing fonts..."
+    mkdir -p "$font_dir"
+    rm -rfv "$font_dir/*"
 
     if which fc-cache; then
-        echo 'Installing powerline-patched-font'
+        echo 'Installing powerline-patched-font...'
        
+        if [[ -d "$temp" ]]; then
+            rm -rfv "$temp"
+        fi            
+
         # download powerline fonts
-        git clone https://github.com/powerline/fonts $HOME/powerline-font-temp
+        git clone --depth 1 https://github.com/powerline/fonts "$temp"
         
         # need these for powerlevel10k theme
         download-meslo-fonts
 
-        find $HOME/powerline-font-temp -regextype posix-extended -iregex '.*\.(otf|ttf)' -print0 | xargs -0 -I % mv -v % $HOME/.fonts/
-        rm -rfv $HOME/powerline-font-temp
-        fc-cache -vf $HOME/.fonts/
+        find "$temp" -regextype posix-extended -iregex '.*\.(otf|ttf)' -print0 | xargs -0 -I % mv -v % "$font_dir"
+        rm -rfv "$temp"
+        fc-cache -vf "$font_dir"
     fi
 }
