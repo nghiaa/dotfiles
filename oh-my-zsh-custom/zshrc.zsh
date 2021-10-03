e18a4e83fdc3578bbbe4fd7b8c6f74ae2d3e7020
nghiaa <nghiatruong.dev@gmail.com>
2021-10-03 11:56:18 +0700

    add zsh alias configs

--- oh-my-zsh-custom/zshrc.zsh
+++ oh-my-zsh-custom/zshrc.zsh
@@ -1,17 +1,15 @@
 #!/usr/bin/env zsh
 
-# bindkey
-bindkey '' backward-delete-char
-bindkey '^h' backward-delete-char
-bindkey '^w' backward-kill-word
-bindkey '^r' history-incremental-search-backward
-export KEYTIMEOUT=1
+# options
+setopt NO_clobber
+setopt NO_beep
 
 # aliases
-# alias ln='nocorrect ln'
-# alias lsa='ls -alkiGF'
-# alias rm='rm -r'
-# alias cp='cp -vr'
+if [[ -f "$HOME/.oh-my-zsh/custom/.zshrc.alias" ]]; then
+    while read -r line || [[ -n "$line" ]]; do
+        eval "alias $line"
+    done < "$HOME/.oh-my-zsh/custom/.zshrc.alias"
+fi
 
 if which fzf > /dev/null; then
     alias fzfp='fzf --preview '"'"'[[ $(file --mime {}) =~ binary ]] &&
@@ -22,10 +20,9 @@ if which fzf > /dev/null; then
         cat {}) 2> /dev/null | head -500'"'"
 fi
 
-export FZF_COMPLETION_TRIGGER=',,'
 [[ -f "$HOME/.fzf.zsh" ]] && source $HOME/.fzf.zsh
 
-# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+# enable powerlevel10k theme, run `p10k configure` or edit ~/.p10k.zsh to customize prompt.
 [[ -f "$HOME/.p10k.zsh" ]] && source $HOME/.p10k.zsh
 
 source $HOME/.oh-my-zsh/custom/plugins/zsh-histdb/sqlite-history.zsh
